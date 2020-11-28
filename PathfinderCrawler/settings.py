ef6c4d4d66d28848dc77bb6297adbbc30e2202c1
Nicola <nic.ferrari.19@gmail.com>
2020-11-28 20:44:14 +0100

    Added logging on file, setted call delay at 0.25s

--- PathfinderCrawler/settings.py
+++ PathfinderCrawler/settings.py
@@ -19,13 +19,16 @@ NEWSPIDER_MODULE = 'PathfinderCrawler.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+#Log file
+LOG_FILE = "logs/crawler.log"
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.25    # 250 ms of delay
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
