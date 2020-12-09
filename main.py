a61ebf0e7f0c4b85d5dc7c9b3426a051a2b4fe1f
Nicola <nic.ferrari.19@gmail.com>
2020-12-09 19:35:37 +0100

    added documents.7z

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-from scrapy import cmdline
-cmdline.execute("scrapy crawl pfsrd".split())
+#from scrapy import cmdline
+#cmdline.execute("scrapy crawl pfsrd".split())
+
+class A:
+	def __getattribute__(self,x):
+		try:
+			return self.__dict__[x]
+		except AttributeError:
+			self.__dict__[x] = None
+
+a = A()
+a.foo
