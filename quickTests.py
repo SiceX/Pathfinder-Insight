0b0174efaf29d024bb3e3746aa97cfffcaaf3341
Nicola <nic.ferrari.19@gmail.com>
2021-02-07 21:01:56 +0100

    Distilled a word dictionary from the index, but not used

--- quickTests.py
+++ quickTests.py
@@ -4,25 +4,25 @@ import os
 #from pdfreader import SimplePDFViewer
 
 #cmdline.execute("scrapy crawl pfsrd".split())
-import nltk
-from nltk.corpus import stopwords
+#import nltk
+#from nltk.corpus import stopwords
 
-
-def tokenize(text):
-	# tokenizzazione
-	tokens = nltk.wordpunct_tokenize(text)
-
-
-	# Eliminazione di stopwords e punteggiatura
-	tokens2 = []
-	for t in tokens:
-		if t not in stopwords.words('english') and t.isalnum():
-			tokens2.append(t)
-	return tokens2
-
-
-
-print(tokenize("basics-ability-scores"))
+#
+# def tokenize(text):
+# 	# tokenizzazione
+# 	tokens = nltk.wordpunct_tokenize(text)
+#
+#
+# 	# Eliminazione di stopwords e punteggiatura
+# 	tokens2 = []
+# 	for t in tokens:
+# 		if t not in stopwords.words('english') and t.isalnum():
+# 			tokens2.append(t)
+# 	return tokens2
+#
+#
+#
+# print(tokenize("basics-ability-scores"))
 
 
 #extract text content from .pdf files and generate it's json file
@@ -50,4 +50,21 @@ print(tokenize("basics-ability-scores"))
 #
 #     with open(f'{root}\\{filename}.json', 'x') as newFile:
 #         json.dump(jsonFile, newFile)
+
+# from whoosh import index
+# ix = index.open_dir("indexdir")
+# searcher = ix.searcher()
+# #list(searcher.lexicon("procContent"))
+# with open("Preprocessing/pathfinderWords.txt", 'w') as file:
+# 	lexicon = list(searcher.lexicon("procContent"))
+# 	lessicono = ""
+# 	for e in lexicon:
+# 		if e.decode("utf-8").isalpha():
+# 			lessicono = f'{lessicono} {e.decode("utf-8")}'
+# 	#lexicon = ' '.join(e) for e in lexicon
+# 	#print(lexicon)
+# 	file.write(lessicono)
+
+# from autocorrect.word_count import count_words
+# count_words("utils/pathfinderWords.txt", 'en')
 
